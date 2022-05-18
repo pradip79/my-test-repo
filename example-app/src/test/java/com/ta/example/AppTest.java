bddbd5a63c1ae315c1ad7859a830f685655f059e
Pradip patra <pradip.patra@gmail.com>
2022-05-18 22:31:54 +0530

    added java classes

---
+++ example-app/src/test/java/com/ta/example/AppTest.java
@@ -0,0 +1,27 @@
+package com.ta.example;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertFalse( true );
+    }
+}
